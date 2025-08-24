328a4514ffb61517b2b4a68af6601f4a1ca649ac
PPiovan <pablopiovan94@gmail.com>
2025-08-24 18:03:01 -0300

    Actualizacion sin JS

--- main.js
+++
@@ -1,6 +0,0 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error);
-} else {
-  // La geolocalización no es compatible con este navegador.
-  console.log("La geolocalización no es compatible con este navegador.");
-}
