b722d9707ddcaeae40b8f65c8bec7a71ed8dce5d
EmanuelAguirre818 <emanuelaguirre818@gmail.com>
2025-09-30 19:36:30 -0300

    Implementando modo oscuro (solo home)

--- JS/main.js
+++ JS/main.js
@@ -182,3 +182,33 @@ document.querySelectorAll('.nav-list a').forEach(link => {
     navList.classList.remove('active');
   });
 });
+
+const toggleDarkMode = document.querySelector('.logo-image')
+const bodyContainer = document.querySelector('.body-container')
+
+let temaActual = localStorage.getItem('tema') || 'claro'
+bodyContainer.classList.remove('claro', 'oscuro')
+bodyContainer.classList.add(temaActual)
+
+// también seteo la imagen según el tema guardado
+if (temaActual === 'oscuro') {
+    toggleDarkMode.src = "imgs/icons/despejado-noche.png"  // ruta para modo oscuro
+} else {
+    toggleDarkMode.src = "imgs/icons/despejado.png"    // ruta para modo claro
+}
+
+toggleDarkMode.addEventListener('click', () => {
+    if (temaActual === 'claro') {
+        temaActual = 'oscuro'
+        toggleDarkMode.src = "imgs/icons/despejado-noche.png"
+    } else {
+        temaActual = 'claro'
+        toggleDarkMode.src = "imgs/icons/despejado.png"
+    }
+
+    bodyContainer.classList.remove('claro', 'oscuro')
+    bodyContainer.classList.add(temaActual)
+
+    localStorage.setItem('tema', temaActual)
+})
+
